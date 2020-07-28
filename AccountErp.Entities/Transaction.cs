ef32d2877af649832442b6680de7f8432a61a022
Ehetesham Ali <ali.ehetesham909@gmail.com>
2020-07-28 15:42:41 +0530

    Ehtesham :- 2020/07/28

--- AccountErp.Entities/Transaction.cs
+++ AccountErp.Entities/Transaction.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static AccountErp.Utilities.Constants;
 
 namespace AccountErp.Entities
 {
     public class Transaction
     {
         public int Id { get; set; }
-        public int ContractId { get; set; }
-        public int DebitAccId { get; set; }
-        public int CreditAccId { get; set; }
+        public int TransactionId { get; set; }
+        public int CompanyId { get; set; }
+        public int AccountId { get; set; }
+        public int ContactId { get; set; }
         public int TransactionTypeId { get; set; }
-        public int PaymentTypeId { get; set; }
+        public string TransactionNumber { get; set; }
         public string Description { get; set; }
-        public DateTime TranscDate { get; set; }
-        public Decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal DebitAmount { get; set; }
+        public decimal CreditAmount { get; set; }
+        public DateTime CreaionDate { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public ContactType ContactType { get; set; }
         public int Status { get; set; }
 
 
