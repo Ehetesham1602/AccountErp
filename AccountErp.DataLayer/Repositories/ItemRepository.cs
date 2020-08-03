36b42ca7240c0f25ff3453bbb59a8a539a3d39df
Ehetesham Ali <ali.ehetesham909@gmail.com>
2020-08-04 01:07:23 +0530

    Ehetesham:-ItemChange04Aug

--- AccountErp.DataLayer/Repositories/ItemRepository.cs
+++ AccountErp.DataLayer/Repositories/ItemRepository.cs
@@ -67,6 +67,8 @@ namespace AccountErp.DataLayer.Repositories
             return await (from s in _dataContext.Items
                           join c in _dataContext.SalesTaxes
                           on s.SalesTaxId equals c.Id
+                         into groupjoin_Sales
+                           from c in groupjoin_Sales.DefaultIfEmpty()
                           where status == null
                             ? s.Status != Constants.RecordStatus.Deleted
                             : s.Status == status.Value
